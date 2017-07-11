0b6a22e4523affb945d2d9cc28f2d60a014aac48
bfidans1@binghamton.edu <bfidans1@binghamton.com>
2017-07-11 19:23:10 +0300

    Devise completed, paperclip added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
 u = User.create(email: "admin@admin.com", password: 123456)
 
 c1 = Category.create(title: "Genel", text: 'Genel kategorimiz burasıdır.')
@@ -13,4 +12,4 @@ c2 = Category.create(title: "RoR", text: 'Ruby on Rails ile ilgili fikirlerin bu
 
 Post.create(title: 'Züper Fikir', text: 'Über Züper Fikir', category: c1, user: u)
 Post.create(title: 'Süper Fikir!', text: 'Bence bilişim terimleri tekrar düzenlensin.', category_id: c1.id, user: u)
-Post.create(title: 'Deneme Fikir', text: 'Test textım.', category:   c2, user: u)
+Post.create(title: 'Deneme Fikir', text: 'Test descriptionriptionım.', category: c2, user: u)
